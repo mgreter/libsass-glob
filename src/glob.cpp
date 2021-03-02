42b767c668a78fc863606ae2d2dde3f5ee699325
Marcel Greter <marcel.greter@ocbnet.ch>
2021-03-02 17:13:48 +0100

    Adjust for latest libsass 4.0 branch

--- src/glob.cpp
+++ src/glob.cpp
@@ -13,14 +13,16 @@ extern "C" const char* ADDCALL libsass_get_version() {
 }
 
 // create a custom importer to resolve glob-based includes
-Sass_Import_List glob_importer(const char* cur_path, Sass_Importer_Entry cb, struct Sass_Compiler* comp)
+struct SassImportList* glob_importer(const char* cur_path, struct SassImporter* cb, struct SassCompiler* comp)
 {
 
   // get the base directory from previous import
-  Sass_Import_Entry imp = sass_compiler_get_last_import(comp);
+  const struct SassImport* imp = sass_compiler_get_last_import(comp);
   char* prev = strdup(sass_import_get_abs_path(imp));
   std::string pattern(dirname(prev)); std::free(prev);
-  pattern += std::string("/") + cur_path;
+  if (strcmp(pattern.c_str(), "stream:") == 0) pattern = "";
+  else pattern += std::string("/");
+  pattern += cur_path;
 
   // instantiate the matcher instance
   FS::Match matcher(pattern);
@@ -33,30 +35,28 @@ Sass_Import_List glob_importer(const char* cur_path, Sass_Importer_Entry cb, str
   // get the cookie from importer descriptor
   // void* cookie = sass_importer_get_cookie(cb);
   // create a list to hold our import entries
-  Sass_Import_List incs = sass_make_import_list(matches.size());
+  struct SassImportList* incs = sass_make_import_list();
 
   // iterate over the list and print out the results
   for (size_t i = 0; i < matches.size(); ++i) {
     // create intermediate string object
     std::string path = matches[i]->path();
-    // create the resolved import entries (paths to be loaded)
-    incs[i] = sass_make_import(path.c_str(), path.c_str(), 0, 0);
+    // create the resolved import paths to load
+    sass_import_list_push(incs, sass_make_import(
+      path.c_str(), path.c_str(),
+      0, 0, SASS_IMPORT_AUTO));
   }
 
   // return imports
   return incs;
 }
 
-// entry point for libsass to request custom importers from plugin
-extern "C" Sass_Importer_List ADDCALL libsass_load_importers()
+// entry point for libsass to request custom functions from plugin
+extern "C" void ADDCALL libsass_init_plugin(struct SassCompiler* compiler)
 {
-  // allocate a custom function caller
-  Sass_Importer_Entry c_header =
-	sass_make_importer(glob_importer, 3000, (void*) 0);
-  // create list of all custom functions
-  Sass_Importer_List imp_list = sass_make_importer_list(1);
-  // put the only function in this plugin to the list
-  sass_importer_set_list_entry(imp_list, 0, c_header);
-  // return the list
-  return imp_list;
+
+  // Add constants via custom headers
+  sass_compiler_add_custom_importer(compiler,
+    sass_make_importer(glob_importer, 3000, (void*)0));
+
 }
